592578aca3a929cb6c868a6929a90adeba71cfd5
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-07-11 11:34:40 +0100

    the previous design direction was discussed and scrapped

--- app/_components/non-reusable/LandingBackgroundImage.js
+++ app/_components/non-reusable/LandingBackgroundImage.js
@@ -1,9 +1,8 @@
 import Image from 'next/image';
 
 import bg from '@/public/landing-bg.jpg';
-import DodgingSvg from '../reusable/DodgingSvg';
 
-function LandingBackgroundImage({ dodge = false }) {
+function LandingBackgroundImage() {
   return (
     <div className="absolute top-0 left-0 w-full h-full max-h-screen bg-gradient-to-b from-transparent to-space-950 to-85%">
       <Image
@@ -14,8 +13,6 @@ function LandingBackgroundImage({ dodge = false }) {
         quality={80}
         className="top-0 left-0 object-cover object-left w-full pointer-events-none -z-20"
       />
-
-      {dodge && <DodgingSvg />}
     </div>
   );
 }
