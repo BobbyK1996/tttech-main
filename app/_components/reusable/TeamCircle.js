7392a10e96f71cb62761d9382f02ca1636ac7650
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-08-24 23:29:09 +0100

    Used ...Array(7) for a quick mock up. Changed it so that the items in the circle is based off an array. Will have this array be in a database and passed through as a component for easy use

--- app/_components/reusable/TeamCircle.js
+++ app/_components/reusable/TeamCircle.js
@@ -5,6 +5,58 @@ import { useEffect, useRef } from 'react';
 import trishnaMin from '@/public/trishna-min.jpg';
 import Image from 'next/image';
 
+const employeeArray = [
+  {
+    name: 'Trishna',
+    image: trishnaMin,
+    role: 'CEO',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto assumenda veritatis nostrum qui libero quaerat temporibus ratione corporis, provident recusandae consectetur! Vel asperiores maiores ab eius quod veniam iusto voluptas?',
+  },
+  {
+    name: 'Trishna',
+    image: trishnaMin,
+    role: 'CEO',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto assumenda veritatis nostrum qui libero quaerat temporibus ratione corporis, provident recusandae consectetur! Vel asperiores maiores ab eius quod veniam iusto voluptas?',
+  },
+  {
+    name: 'Trishna',
+    image: trishnaMin,
+    role: 'CEO',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto assumenda veritatis nostrum qui libero quaerat temporibus ratione corporis, provident recusandae consectetur! Vel asperiores maiores ab eius quod veniam iusto voluptas?',
+  },
+  {
+    name: 'Trishna',
+    image: trishnaMin,
+    role: 'CEO',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto assumenda veritatis nostrum qui libero quaerat temporibus ratione corporis, provident recusandae consectetur! Vel asperiores maiores ab eius quod veniam iusto voluptas?',
+  },
+  {
+    name: 'Trishna',
+    image: trishnaMin,
+    role: 'CEO',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto assumenda veritatis nostrum qui libero quaerat temporibus ratione corporis, provident recusandae consectetur! Vel asperiores maiores ab eius quod veniam iusto voluptas?',
+  },
+  {
+    name: 'Trishna',
+    image: trishnaMin,
+    role: 'CEO',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto assumenda veritatis nostrum qui libero quaerat temporibus ratione corporis, provident recusandae consectetur! Vel asperiores maiores ab eius quod veniam iusto voluptas?',
+  },
+  {
+    name: 'Trishna',
+    image: trishnaMin,
+    role: 'CEO',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto assumenda veritatis nostrum qui libero quaerat temporibus ratione corporis, provident recusandae consectetur! Vel asperiores maiores ab eius quod veniam iusto voluptas?',
+  },
+];
+
 function TeamCircle() {
   const parentRef = useRef(null);
 
@@ -37,15 +89,15 @@ function TeamCircle() {
     >
       <div className="relative w-full border-2 border-white rounded-full squareAspectRatio">
         <div className="relative flex items-center justify-center w-full h-full cursor-pointer -left-1/2">
-          {[...Array(7)].map((_, i) => (
+          {employeeArray.map((employee, i) => (
             <div
               key={i}
               style={{ '--i': i }}
               className="absolute w-20 h-20 duration-500 border border-white rounded-full shadow-lg rotate-i"
             >
               <Image
-                src={trishnaMin}
-                alt="image"
+                src={employee.image}
+                alt={employee.name}
                 className="absolute top-0 left-0 object-cover w-full h-full rounded-full"
               />
             </div>
