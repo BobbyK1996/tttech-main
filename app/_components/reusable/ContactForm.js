1d12ae9fb8227fef329f87ec5ac992be57b876f1
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-09-15 22:29:33 +0100

    Used the nodemailer documentation to setup a barebones email system for ContactForm. Next step is to set up the ContactForm and structure an email template

--- app/_components/reusable/ContactForm.js
+++ app/_components/reusable/ContactForm.js
@@ -1,13 +1,24 @@
-'use client';
+import { sendMail } from '@lib/mail';
 
 function ContactForm() {
+  const send = async () => {
+    'use server';
+    await sendMail({
+      to: 'freestuffpls12345@gmail.com',
+      name: 'Bobby',
+      subject: 'Test mail',
+      body: '<h1>Testing</h1>',
+    });
+  };
+
   return (
     <form className="w-full">
       <button
-        onClick={(e) => {
-          e.preventDefault();
-          console.log('hello');
-        }}
+        // onClick={(e) => {
+        //   e.preventDefault();
+        //   console.log('hello');
+        // }}
+        formAction={send}
       >
         Test
       </button>
---
+++ app/_lib/mail.js
@@ -0,0 +1,41 @@
+import nodemailer from 'nodemailer';
+import { validateString } from '@lib/helperShared';
+
+export async function sendMail({ to, name, subject, body }) {
+  validateString(to, 'To');
+  validateString(name, 'Name');
+  validateString(subject, 'Subject');
+  validateString(body, 'Body');
+
+  const { SMTP_EMAIL, SMTP_PASSWORD } = process.env;
+
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: SMTP_EMAIL,
+      pass: SMTP_PASSWORD,
+    },
+  });
+
+  try {
+    const testResult = await transport.verify();
+    console.log(testResult);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  try {
+    const sendResult = await transport.sendMail({
+      from: SMTP_EMAIL,
+      to,
+      subject,
+      html: body,
+    });
+
+    console.log(sendResult);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
