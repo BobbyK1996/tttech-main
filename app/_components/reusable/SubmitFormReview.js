e6edc23c0de739149d301194db729ab0b632e183
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-10-04 16:30:32 +0100

    Made it more obvious which categories are required and optional in SubmitFormReview

--- app/_components/reusable/SubmitFormReview.js
+++ app/_components/reusable/SubmitFormReview.js
@@ -33,162 +33,166 @@ function SubmitFormReview({ formData }) {
     );
 
   return (
-    <div className='grid grid-cols-[0.15fr,0.25fr,2.5fr,0.5fr] gap-x-2 gap-y-4 text-white sm:gap-x-4 lg:grid-cols-[0.1fr,0.25fr,2.5fr,0.5fr]'>
-      {/* Name */}
-      <div
-        className={`${reviewItemStyles} ${isValidName(formDataTrimmed.givenName) && isValidName(formDataTrimmed.surname) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        <IoPersonCircleSharp />
-      </div>
-      <div
-        className={`${isValidName(formDataTrimmed.givenName) && isValidName(formDataTrimmed.surname) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        Name*:
-      </div>
-      <div>
-        {formDataTrimmed.givenName && formDataTrimmed.surname
-          ? `${formDataTrimmed.givenName} ${formDataTrimmed.surname}`
-          : '-'}
-      </div>
-      <div className={reviewItemStyles}>
-        {isValidName(formDataTrimmed.givenName) &&
-        isValidName(formDataTrimmed.surname) ? (
-          <IoCheckmarkCircle className='text-primary-500' />
-        ) : (
-          <IoCloseCircle className='text-red-500' />
-        )}
+    <div className='text-white'>
+      <h1 className='mb-2 text-2xl font-bold text-accent-500 underline'>
+        Required
+      </h1>
+      <div className='grid grid-cols-[0.15fr,0.25fr,2.5fr,0.5fr] gap-x-2 gap-y-4 sm:gap-x-4 lg:grid-cols-[0.1fr,0.25fr,2.5fr,0.5fr]'>
+        <div
+          className={`${reviewItemStyles} ${isValidName(formDataTrimmed.givenName) && isValidName(formDataTrimmed.surname) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          <IoPersonCircleSharp />
+        </div>
+        <div
+          className={`${isValidName(formDataTrimmed.givenName) && isValidName(formDataTrimmed.surname) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          Name:
+        </div>
+        <div>
+          {formDataTrimmed.givenName && formDataTrimmed.surname
+            ? `${formDataTrimmed.givenName} ${formDataTrimmed.surname}`
+            : '-'}
+        </div>
+        <div className={reviewItemStyles}>
+          {isValidName(formDataTrimmed.givenName) &&
+          isValidName(formDataTrimmed.surname) ? (
+            <IoCheckmarkCircle className='text-primary-500' />
+          ) : (
+            <IoCloseCircle className='text-red-500' />
+          )}
+        </div>
+        <div
+          className={`${reviewItemStyles} ${isValidPhoneNumber(formDataTrimmed.number) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          <IoPhonePortrait />
+        </div>
+        <div
+          className={`${isValidPhoneNumber(formDataTrimmed.number) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          Number:
+        </div>
+        <div>{formDataTrimmed.number ? `${formDataTrimmed.number}` : '-'}</div>
+        <div className={reviewItemStyles}>
+          {isValidPhoneNumber(formDataTrimmed.number) ? (
+            <IoCheckmarkCircle className='text-primary-500' />
+          ) : (
+            <IoCloseCircle className='text-red-500' />
+          )}
+        </div>
+
+        <div
+          className={`${reviewItemStyles} ${isValidEmail(formDataTrimmed.email) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          <IoMail />
+        </div>
+        <div
+          className={`${isValidEmail(formDataTrimmed.email) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          Email:
+        </div>
+        <div>{formDataTrimmed.email ? `${formDataTrimmed.email}` : '-'}</div>
+        <div className={reviewItemStyles}>
+          {isValidEmail(formDataTrimmed.email) ? (
+            <IoCheckmarkCircle className='text-primary-500' />
+          ) : (
+            <IoCloseCircle className='text-red-500' />
+          )}
+        </div>
       </div>
 
-      {/* Phone Number */}
-      <div
-        className={`${reviewItemStyles} ${isValidPhoneNumber(formDataTrimmed.number) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        <IoPhonePortrait />
-      </div>
-      <div
-        className={`${isValidPhoneNumber(formDataTrimmed.number) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        Number*:
-      </div>
-      <div>{formDataTrimmed.number ? `${formDataTrimmed.number}` : '-'}</div>
-      <div className={reviewItemStyles}>
-        {isValidPhoneNumber(formDataTrimmed.number) ? (
-          <IoCheckmarkCircle className='text-primary-500' />
-        ) : (
-          <IoCloseCircle className='text-red-500' />
-        )}
-      </div>
+      <h1 className='mb-2 mt-6 text-2xl font-bold text-accent-500 underline'>
+        Optional
+      </h1>
+      <div className='grid grid-cols-[0.15fr,0.25fr,2.5fr,0.5fr] gap-x-2 gap-y-4 sm:gap-x-4 lg:grid-cols-[0.1fr,0.25fr,2.5fr,0.5fr]'>
+        <div
+          className={`${reviewItemStyles} ${isValidJobTitle(formDataTrimmed.currentJobTitle) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          <IoBriefcase />
+        </div>
+        <div
+          className={`${isValidJobTitle(formDataTrimmed.currentJobTitle) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          Current Job Title:
+        </div>
+        <div>
+          {formDataTrimmed.currentJobTitle
+            ? `${formDataTrimmed.currentJobTitle}`
+            : '-'}
+        </div>
+        <div className={reviewItemStyles}>
+          {isValidJobTitle(formDataTrimmed.currentJobTitle) ? (
+            <IoCheckmarkCircle className='text-primary-500' />
+          ) : (
+            <IoCloseCircle className='text-red-500' />
+          )}
+        </div>
 
-      {/* Email */}
-      <div
-        className={`${reviewItemStyles} ${isValidEmail(formDataTrimmed.email) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        <IoMail />
-      </div>
-      <div
-        className={`${isValidEmail(formDataTrimmed.email) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        Email*:
-      </div>
-      <div>{formDataTrimmed.email ? `${formDataTrimmed.email}` : '-'}</div>
-      <div className={reviewItemStyles}>
-        {isValidEmail(formDataTrimmed.email) ? (
-          <IoCheckmarkCircle className='text-primary-500' />
-        ) : (
-          <IoCloseCircle className='text-red-500' />
-        )}
-      </div>
+        <div
+          className={`${reviewItemStyles} ${isValidUrl(formDataTrimmed.linkedinLink, true) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          <IoLogoLinkedin />
+        </div>
+        <div
+          className={`${isValidUrl(formDataTrimmed.linkedinLink, true) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          LinkedIn:
+        </div>
+        <div>
+          {formDataTrimmed.linkedinLink
+            ? `${formDataTrimmed.linkedinLink}`
+            : '-'}
+        </div>
+        <div className={reviewItemStyles}>
+          {isValidUrl(formDataTrimmed.linkedinLink, true) ? (
+            <IoCheckmarkCircle className='text-primary-500' />
+          ) : (
+            <IoCloseCircle className='text-red-500' />
+          )}
+        </div>
 
-      {/* Current Job Title */}
-      <div
-        className={`${reviewItemStyles} ${isValidJobTitle(formDataTrimmed.currentJobTitle) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        <IoBriefcase />
-      </div>
-      <div
-        className={`${isValidJobTitle(formDataTrimmed.currentJobTitle) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        Current Job Title:
-      </div>
-      <div>
-        {formDataTrimmed.currentJobTitle
-          ? `${formDataTrimmed.currentJobTitle}`
-          : '-'}
-      </div>
-      <div className={reviewItemStyles}>
-        {isValidJobTitle(formDataTrimmed.currentJobTitle) ? (
-          <IoCheckmarkCircle className='text-primary-500' />
-        ) : (
-          <IoCloseCircle className='text-red-500' />
-        )}
-      </div>
+        <div
+          className={`${reviewItemStyles} ${isValidUrl(formDataTrimmed.portfolioLink) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          <IoLinkSharp />
+        </div>
+        <div
+          className={`${isValidUrl(formDataTrimmed.portfolioLink) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          Portfolio:
+        </div>
+        <div>
+          {formDataTrimmed.portfolioLink
+            ? `${formDataTrimmed.portfolioLink}`
+            : '-'}
+        </div>
+        <div className={reviewItemStyles}>
+          {isValidUrl(formDataTrimmed.portfolioLink, true) ? (
+            <IoCheckmarkCircle className='text-primary-500' />
+          ) : (
+            <IoCloseCircle className='text-red-500' />
+          )}
+        </div>
 
-      {/* LinkedIn */}
-      <div
-        className={`${reviewItemStyles} ${isValidUrl(formDataTrimmed.linkedinLink, true) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        <IoLogoLinkedin />
-      </div>
-      <div
-        className={`${isValidUrl(formDataTrimmed.linkedinLink, true) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        LinkedIn:
-      </div>
-      <div>
-        {formDataTrimmed.linkedinLink ? `${formDataTrimmed.linkedinLink}` : '-'}
-      </div>
-      <div className={reviewItemStyles}>
-        {isValidUrl(formDataTrimmed.linkedinLink, true) ? (
-          <IoCheckmarkCircle className='text-primary-500' />
-        ) : (
-          <IoCloseCircle className='text-red-500' />
-        )}
-      </div>
-
-      {/* Portfolio */}
-      <div
-        className={`${reviewItemStyles} ${isValidUrl(formDataTrimmed.portfolioLink) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        <IoLinkSharp />
-      </div>
-      <div
-        className={`${isValidUrl(formDataTrimmed.portfolioLink) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        Portfolio:
-      </div>
-      <div>
-        {formDataTrimmed.portfolioLink
-          ? `${formDataTrimmed.portfolioLink}`
-          : '-'}
-      </div>
-      <div className={reviewItemStyles}>
-        {isValidUrl(formDataTrimmed.portfolioLink, true) ? (
-          <IoCheckmarkCircle className='text-primary-500' />
-        ) : (
-          <IoCloseCircle className='text-red-500' />
-        )}
-      </div>
-
-      {/* Message */}
-      <div
-        className={`${reviewItemStyles} ${isValidMessage(formDataTrimmed.message) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        <IoReader />
-      </div>
-      <div
-        className={`${isValidMessage(formDataTrimmed.message) ? 'text-primary-500' : 'text-red-500'}`}
-      >
-        Message:
-      </div>
-      <div className='max-h-40 overflow-y-scroll'>
-        {formDataTrimmed.message ? `${formDataTrimmed.message}` : '-'}
-      </div>
-      <div className={reviewItemStyles}>
-        {isValidMessage(formDataTrimmed.linkedinLink) ? (
-          <IoCheckmarkCircle className='text-primary-500' />
-        ) : (
-          <IoCloseCircle className='text-red-500' />
-        )}
+        <div
+          className={`${reviewItemStyles} ${isValidMessage(formDataTrimmed.message) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          <IoReader />
+        </div>
+        <div
+          className={`${isValidMessage(formDataTrimmed.message) ? 'text-primary-500' : 'text-red-500'}`}
+        >
+          Message:
+        </div>
+        <div className='max-h-40 overflow-y-scroll'>
+          {formDataTrimmed.message ? `${formDataTrimmed.message}` : '-'}
+        </div>
+        <div className={reviewItemStyles}>
+          {isValidMessage(formDataTrimmed.linkedinLink) ? (
+            <IoCheckmarkCircle className='text-primary-500' />
+          ) : (
+            <IoCloseCircle className='text-red-500' />
+          )}
+        </div>
       </div>
     </div>
   );
